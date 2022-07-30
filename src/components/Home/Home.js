adf5f85449cb57f328738e98baa6f11c99eb1d13
Adam MacDonald <am1macdonald@gmail.com>
2022-07-30 11:32:41 -0300

    move components to sub-folders

---
+++ src/components/Home/Home.js
@@ -0,0 +1,25 @@
+import React from "react";
+import heroImg from "../../assets/MegaLo.webp";
+
+const Home = () => {
+  return (
+    <div className="container">
+      <h1>Welcome to Mega-Lo-Mart</h1>
+      <img src={heroImg} alt="Mego Lo Mart, Arlen Branch" />
+      <blockquote>
+        At Mega Lo Mart, you're shopping for the rest of your life.
+      </blockquote>
+      <div className="flex-row">
+        <div>
+          <h6>Featured Sale</h6>
+          <p></p>
+        </div>
+        <div>
+          <img src="" alt="" />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
