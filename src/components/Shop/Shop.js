086d6d3c1dc3f78983fb210f21dc8cfb8a1447f2
am1macdonald <am1macdonald@gmail.com>
2022-08-06 07:32:52 -0400

    add default to select

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -20,35 +20,29 @@ import "simplebar/dist/simplebar.min.css";
 // };
 
 const Shop = (props) => {
-
   const handleChange = (e) => {
     props.sort(e.target.value);
-  }
+  };
 
   return (
     <div className="page">
-      <div className={`flex-row space-between ${styles.shopFlex}`}>
-        <div className={`container ${styles.left}`}>
-          <span>Filters</span>
-        </div>
+      <div className={`container`}>
+        <span className={`flex-row ${styles.span}`}>
+          <label htmlFor="sort">sort:</label>
+          <select name="sort" id="sort" onChange={handleChange}>
+            <option value="null">select</option>
+            <option value="alphabetical">alphabetical</option>
+            <option value="price asc">price ↑</option>
+            <option value="price desc">price ↓</option>
+          </select>
+        </span>
 
-        <div className={`container ${styles.right}`}>
-          <span className="flex-row">
-            <label htmlFor="sort">sort:</label>
-            <select name="sort" id="sort" onChange={handleChange}>
-              <option value="alphabetical">alphabetical</option>
-              <option value="price asc">price ↑</option>
-              <option value="price desc">price ↓</option>
-            </select>
-          </span>
-
-          {props.loading && <div>Loading</div>}
-          {!props.loading && (
-            <SimpleBar className={styles.simpleBar}>
-              <ul className={`${styles.itemGrid}`}>{props.children}</ul>
-            </SimpleBar>
-          )}
-        </div>
+        {props.loading && <div>Loading</div>}
+        {!props.loading && (
+          <SimpleBar className={styles.simpleBar}>
+            <ul className={`${styles.itemGrid}`}>{props.children}</ul>
+          </SimpleBar>
+        )}
       </div>
     </div>
   );
