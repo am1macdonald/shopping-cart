00c797406066964bbc486d734be170315a212030
Adam MacDonald <am1macdonald@gmail.com>
2022-08-06 15:52:14 -0400

    build out cart

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,16 +1,25 @@
 import React from "react";
+import SimpleBar from "simplebar-react";
+import "simplebar/dist/simplebar.min.css";
+import styles from "./cart.module.scss";
 
-const Cart = ({items}) => {
-
-  
-
+const Cart = ({ children, totalCost }) => {
   return (
-    <div>
-      <h1>Cart</h1>
-      <div></div>
-      <div>Totals:</div>
+    <div className={`page ${styles.shopPage}`}>
+      <div className={`container`}>
+        <div className={`${styles.leftAlign} border-bottom`}>
+          <h3>Your Cart</h3>
+        </div>
+
+        <SimpleBar className="simple-bar">
+          <ul className={`${styles.cart}`}>{children}</ul>
+        </SimpleBar>
+      </div>
+      <div className={`container ${styles.flexBottom}`}>
+        <p>Total: ${totalCost()}</p>
+      </div>
     </div>
   );
 };
 
-export default Cart
+export default Cart;
