7801aba446d7cf2795b387870a6672ac983b3f2a
Adam MacDonald <am1macdonald@gmail.com>
2022-08-07 17:15:11 -0400

    update tests

--- src/App.test.js
+++ src/App.test.js
@@ -10,20 +10,3 @@ describe("testing app: general", () => {
     expect(app).toBeInTheDocument();
   });
 });
-
-describe("testing navigation bar", () => {
-  it("renders", () => {
-    render(<App />);
-    const navTitle = screen.getByText(/^mega lo mart$/i);
-    expect(navTitle).toBeInTheDocument()
-  });
-});
-
-describe("end to end test", () => {
-  it("navigates", () => {
-    render(<App />);
-
-    const navTabs = screen.getAllByRole("link")
-    console.log(navTabs.length)
-  });
-});
